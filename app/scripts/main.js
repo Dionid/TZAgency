f9142f89c17dde966fb70f4b3ece0d6aba13edf9
Dionid <ditreyw@gmail.com>
2016-09-08 22:36:49 +0300

    Forst commit

---
+++ app/scripts/main.js
@@ -0,0 +1,4 @@
+$(document).ready(function(){
+    $('.section--fullscreen').height($(window).height() - $(window).height()*0.03)
+    $('.portfolio-item').height($('.portfolio-item').width()*0.4)
+});
